e1a777d0146f40377eafd1030dfa3e0a055321ea
onurozkinaci@gmail.com <onurozkinaci@gmail.com>
2023-12-21 00:31:41 +0300

    Add project files.

---
+++ Core/CarBook.Domain/Entities/Car.cs
@@ -0,0 +1,27 @@
+﻿using CarBook.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain.Entites
+{
+    public class Car
+    {
+        public int CarId { get; set; }
+        public int BrandId { get; set; }
+        public Brand Brand { get; set; }
+        public string Model { get; set; }
+        public string CoverImageUrl { get; set; }
+        public int Km { get; set; }
+        public string Transmission { get; set; } //TODO: vites turu - enum olabilir.
+        public byte Seat { get; set; }
+        public byte Luggage { get; set; }
+        public string Fuel { get; set; } //yakit turu
+        public string BigImageUrl { get; set; }
+        public List<CarFeature> CarFeatures { get; set; } //many to many rel.
+        public List<CarDescription> CarDescriptions { get; set; } //one to many rel.
+        public List<CarPricing> CarPricings { get; set; } //many to many rel.
+    }
+}
---
+++ Core/CarBook.Domain/Entities/Pricing.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain.Entities
+{
+    public class Pricing
+    {
+        public int PricingId { get; set; }
+        public string Name { get; set; } //gunluk, haftalik, aylik, yillik, vs. odeme plani
+        public List<CarPricing> CarPricings { get; set; } //many to many rel.
+    }
+}
