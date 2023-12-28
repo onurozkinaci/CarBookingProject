3f6b3753d8d7d3d61360323f954e07c248f4a0ee
onurozkinaci@gmail.com <onurozkinaci@gmail.com>
2023-12-28 23:54:10 +0300

    Persistence and Presentation layers are added. Also, Db context class and first migration is added.

--- Core/CarBook.Domain/Entities/SocialMediaAccount.cs
+++ Core/CarBook.Domain/Entities/SocialMediaAccount.cs
@@ -8,7 +8,7 @@ namespace CarBook.Domain.Entities
 {
     public class SocialMediaAccount
     {
-        public int SocialMediaId { get; set; }
+        public int SocialMediaAccountId { get; set; }
         public string Name { get; set; }
         public string Url { get; set; }
         public string Icon { get; set; }
---
+++ Infrastructure/CarBook.Persistence/Context/CarBookContext.cs
@@ -0,0 +1,37 @@
+﻿using CarBook.Domain.Entites;
+using CarBook.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Persistence.Context
+{
+    public class CarBookContext:DbContext
+    {
+        //=>Baglanti adresine erisim icin kullanilacak. Presentation katmanindaki appsettings.json'da da baglanti adresine erisebiliriz
+        //fakat bunu canliya alimda uygulayacagiz, localde(dev modda) buradan hareket edecegiz;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-IKJ8SQV\\SQLEXPRESS;initial Catalog=CarBookDb; integrated Security=true;TrustServerCertificate=true;");
+        }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Banner> Banners { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<CarFeature> CarFeatures { get; set; }
+        public DbSet<CarDescription> CarDescriptions { get; set; }
+        public DbSet<CarPricing> CarPricings { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<FooterAddress> FooterAddresses { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Pricing> Pricings { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<SocialMediaAccount> SocialMediaAccounts { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
